15ac8921cda18d63107c7679aee9759641ada0e7
kaelzubs <donmart4u@gmail.com>
2022-09-09 16:50:37 +0100

    Afresh 2

--- epredictz/wsgi.py
+++ epredictz/wsgi.py
@@ -4,8 +4,9 @@ from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 
-path = '/Users/robot/Desktop/epredictz/epredictz'
+# path = '/home/kaelzubs/epredictz'
 
+path = 'C:/Users/robot/Desktop/epredictz/epredictz'
 if path not in sys.path:
     sys.path.append(path)
 
@@ -14,5 +15,8 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'epredictz.settings'
 
 application = get_wsgi_application()
 
-application = WhiteNoise(application, root='/Users/robot/Desktop/epredictz/epredictz/static')
-application.add_files('/Users/robot/Desktop/epredictz/epredictz/static/img', prefix='static_img')
+# application = WhiteNoise(application, root='/home/kaelzubs/epredictz/epredictz/static')
+# application.add_files('/home/kaelzubs/epredictz/epredictz/static/img/', prefix='statiic_img')
+
+application = WhiteNoise(application, root='C:/Users/robot/Desktop/epredictz/epredictz/static')
+application.add_files('C:/Users/robot/Desktop/epredictz/epredictz/static/img', prefix='static_img')
