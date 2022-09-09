27f974cddf93c7aeb41b25791c7d511a0ca72f02
kaelzubs <donmart4u@gmail.com>
2022-09-09 18:39:04 +0100

    originals 2

--- epredictz/wsgi.py
+++ epredictz/wsgi.py
@@ -4,7 +4,7 @@ from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
 
 
-path = '/home/kaelzubs/epredictz'
+path = '/epredictz'
 
 # path = 'C:/Users/robot/Desktop/epredictz/epredictz'
 if path not in sys.path:
@@ -15,8 +15,8 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'epredictz.settings'
 
 application = get_wsgi_application()
 
-application = WhiteNoise(application, root='/home/kaelzubs/epredictz/epredictz/static')
-application.add_files('/home/kaelzubs/epredictz/epredictz/static/img/', prefix='statiic_img')
+application = WhiteNoise(application, root='/epredictz/static')
+application.add_files('/epredictz/static/img/', prefix='statiic_img')
 
 # application = WhiteNoise(application, root='C:/Users/robot/Desktop/epredictz/epredictz/static')
 # application.add_files('C:/Users/robot/Desktop/epredictz/epredictz/static/img', prefix='static_img')
