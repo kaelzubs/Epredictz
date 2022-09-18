60b4910c7e46cbcd088fbc382b849633cda4e3bb
kaelzubs <donmart4u@gmail.com>
2022-09-18 20:32:52 +0100

    Sunday Commit last 7

--- epredictz/settings.py
+++ epredictz/settings.py
@@ -73,7 +73,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     "epz1.middleware.WwwRedirectMiddleware",
@@ -85,8 +84,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-# SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
 
 ROOT_URLCONF = 'epredictz.urls'
 
