2551defbdbaec750c95c4a0a677da6b8e878cd02
kaelzubs <donmart4u@gmail.com>
2022-09-20 14:55:53 +0100

    Tuesday again 3

--- epredictz/settings.py
+++ epredictz/settings.py
@@ -82,6 +82,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.gzip.GZipMiddleware',
 ]
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
