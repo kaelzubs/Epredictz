602253b4ac639dbd77be3c351fa3fc78d52ed960
kaelzubs <donmart4u@gmail.com>
2022-09-20 13:09:52 +0100

    Tuesday again 1

--- epz1/urls.py
+++ epz1/urls.py
@@ -1,3 +1,5 @@
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 from django.urls import path
 from .views import list_home
 
@@ -7,4 +9,6 @@ from .views import list_home
 urlpatterns = [
 
     path('', list_home, name='list_home'),
+    path('ads.txt', RedirectView.as_view(url=staticfiles_storage.url("ads.txt")),),
+
 ]
