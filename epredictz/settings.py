62531af44f0909c0331e9ab00e6fa65735c8fd70
kaelzubs <donmart4u@gmail.com>
2022-09-21 13:52:13 +0100

    Wednesday Again 8

--- epredictz/settings.py
+++ epredictz/settings.py
@@ -36,7 +36,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 if DEBUG is False:
     ALLOWED_HOSTS = [
