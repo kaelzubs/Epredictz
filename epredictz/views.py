ac0baaa73379cd476beb4e5d670d88feb67b9e3b
kaelzubs <donmart4u@gmail.com>
2022-09-29 16:09:56 +0100

    Thursday Commit 7

--- epredictz/views.py
+++ epredictz/views.py
@@ -1,7 +1,5 @@
 from django.contrib.sitemaps import Sitemap
 from epz1.models import Home_Page
-from django.http import HttpResponse
-from django.views import View
 
 
 
@@ -11,11 +9,3 @@ class StaticViewSitemap(Sitemap):
 
     def items(self):
         return Home_Page.objects.all()
-
-
-class AdsView(View):
-    """Replace pub-0000000000000000 with your own publisher ID"""
-    line  =  "google.com, pub-6411693288489882, DIRECT, f08c47fec0942fa0"
-
-    def get(self, request, *args, **kwargs):
-        return HttpResponse(line)
