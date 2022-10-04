5be60a7a3ea6dfffb3bec348965f832243adf459
kaelzubs <donmart4u@gmail.com>
2022-10-04 20:27:55 +0100

    Tuesday commit 31

--- epz1/views.py
+++ epz1/views.py
@@ -8,45 +8,45 @@ from ratelimit import limits
 import requests
 
 
-# @limits(calls=10, period=1)
-# def rate_limiter():
-#     url = os.getenv('DAILY_URL')
-#     headers = {
-#         "Content-Type": "application/json",
-#         "Connection": "keep-alive",
-#         "X-RapidAPI-Key": os.getenv('DAILY_RAPIDAPI_KEY'),
-#         "X-RapidAPI-Host": os.getenv('DAILY_RAPIDAPI_HOST')
-#     }
-#     response = requests.request("GET", url, headers=headers)
+@limits(calls=10, period=1)
+def rate_limiter():
+    url = os.getenv('DAILY_URL')
+    headers = {
+        "Content-Type": "application/json",
+        "Connection": "keep-alive",
+        "X-RapidAPI-Key": os.getenv('DAILY_RAPIDAPI_KEY'),
+        "X-RapidAPI-Host": os.getenv('DAILY_RAPIDAPI_HOST')
+    }
+    response = requests.request("GET", url, headers=headers)
     
-#     data = response.json()
-#     arrdata = data['data']
-#     for dt in arrdata:
-#         for res in dt['coupons_list_data']:
-#             if not Home_Page.objects.filter(
-#                 match_dat = res['match_date'],
-#                 match_time = res['match_time'],
-#                 league = res['league_name'],
-#                 home_team = res['home_team'],
-#                 away_team = res['away_team'],
-#                 tip = res['game_prediction'],
-#                 tip_odd = res['odd_value'],
-#                 result = res['match_status']
-#                 ).exists():
-#                 Home_Page.objects.create(
-#                     match_dat = res['match_date'],
-#                     match_time = res['match_time'],
-#                     league = res['league_name'],
-#                     home_team = res['home_team'],
-#                     away_team = res['away_team'],
-#                     tip = res['game_prediction'],
-#                     tip_odd = res['odd_value'],
-#                     result = res['match_status']
-#                 )
+    data = response.json()
+    arrdata = data['data']
+    for dt in arrdata:
+        for res in dt['coupons_list_data']:
+            if not Home_Page.objects.filter(
+                match_dat = res['match_date'],
+                match_time = res['match_time'],
+                league = res['league_name'],
+                home_team = res['home_team'],
+                away_team = res['away_team'],
+                tip = res['game_prediction'],
+                tip_odd = res['odd_value'],
+                result = res['match_status']
+                ).exists():
+                Home_Page.objects.create(
+                    match_dat = res['match_date'],
+                    match_time = res['match_time'],
+                    league = res['league_name'],
+                    home_team = res['home_team'],
+                    away_team = res['away_team'],
+                    tip = res['game_prediction'],
+                    tip_odd = res['odd_value'],
+                    result = res['match_status']
+                )
     
-# rate_limiter()
+rate_limiter()
 
-def detail_home(request, slug):
+def detail_home(request):
     pages = Home_Page.objects.all().order_by('-match_dat')
     if request.user.is_staff or request.user.is_superuser:
         pages = Home_Page.objects.all().order_by('-match_dat')
