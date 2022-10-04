6355a1e22321322bb46d238e6d9c6de7745fac4d
kaelzubs <donmart4u@gmail.com>
2022-10-04 14:11:38 +0100

    Tuesday commit 1

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -41,6 +41,19 @@ $(document).ready(function() {
     };
     // document ready  
 });
+
+
+$(document).ready(function() {
+    setInterval(function() {
+        cache_clear()
+    }, 3000);
+});
+  
+function cache_clear() {
+    window.location.reload(true);
+    // window.location.reload(); use this if you do not remove cache
+}
+
  
  
  
