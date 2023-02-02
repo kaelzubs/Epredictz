3bf1794028d90d558ef004a4a79e5121d2f26401
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-02 23:10:12 +0100

    Update settings.py
    Update debug

--- epredictz/settings.py
+++ epredictz/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 if DEBUG is False:
     ALLOWED_HOSTS = [
