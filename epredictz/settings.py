dfacf3291ec0358bf6fe4923ea96366df975bbe7
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-02 15:59:12 +0100

    Update settings.py
    Db change

--- epredictz/settings.py
+++ epredictz/settings.py
@@ -123,23 +123,9 @@ WSGI_APPLICATION = 'epredictz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-         'ENGINE': 'django.db.backends.sqlite3',
-         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-DATABASES = {
-    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
-}
-
-
-
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
+DATABASES = {}
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 django_heroku.settings(locals())
 
