5b62194ebc7b87aa51f8b2ebeeef4bad5b121522
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-03 10:44:34 +0100

    Update admin.py
    Fix

--- epz5/admin.py
+++ epz5/admin.py
@@ -1,10 +1,4 @@
 from django.contrib import admin
-from . models import faq_Page
 
 # Register your models here.
 
-class DisclaimAdmin(admin.ModelAdmin):
-    list_display = ('title', 'created')
-    search_fields = ('title',)
-
-admin.site.register(faq_Page, DisclaimAdmin)
