00fda0aa62638113b3dbd7b3b0682d799bc68cc1
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-03 01:38:40 +0100

    Update settings.py
    Normal

--- epredictz/settings.py
+++ epredictz/settings.py
@@ -123,10 +123,21 @@ WSGI_APPLICATION = 'epredictz.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Epredictz',
+        'USER': is.getenv('POST_NAME'),
+        'PASSWORD': is.getenv('POST_PASS'),
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+# DATABASES = {}
 # db_from_env = dj_database_url.config(conn_max_age=600)
 # DATABASES['default'].update(db_from_env)
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 django_heroku.settings(locals())
 
