1c9ac46a6bcb6178af5e31fc5c1106c0654e51e5
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-03 10:50:02 +0100

    Update models.py
    Dix

--- epz6/models.py
+++ epz6/models.py
@@ -1,14 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class cookie_Page(models.Model):
-    title = models.CharField(max_length=60)
-    bodytext = models.TextField()
-    created = models.DateTimeField()
-
-    class Meta:
-        ordering = ['-id']
-
-    def __str__(self):
-        return self.title
