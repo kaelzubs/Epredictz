b9bd372b9e7307bee7f45b0aa8b66d6744ced6f8
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-04 23:09:18 +0100

    Update urls.py

--- epz1/urls.py
+++ epz1/urls.py
@@ -3,7 +3,7 @@ from .views import list_home, list_home_prev, list_home_next
 
 urlpatterns = [
 
-    path('', list_home_prev, name='list_home'),
-    path('yesterday/', list_home, name='list_home_prev'),
+    path('yesterday/', list_home_prev, name='list_home_prev'),
+    path('today/', list_home, name='list_home'),
     path('tomorrow/', list_home_next, name='list_home_next')
 ]
