64e98b65997e7feb067469afce017476d0e55c2b
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-04 23:17:29 +0100

    Update views.py

--- epz1/views.py
+++ epz1/views.py
@@ -38,6 +38,39 @@ def list_home_prev(request):
         'forms': forms
     })
 
+def list_home_today(request):
+    pages = Home_Page.objects.order_by("date_time")
+    query = request.GET.get('q')
+    if query:
+        pages = Home_Page.objects.filter(
+            Q(date_time__icontains=query) |
+            Q(league__icontains=query) |
+            Q(home_team__icontains=query) |
+            Q(away_team__icontains=query) |
+            Q(tip__icontains=query) |
+            Q(tip_odd__icontains=query) |
+            Q(result__icontains=query)
+
+        ).distinct()
+
+    paginator = Paginator(pages, 5)
+    page = request.GET.get('page')
+    try:
+        ppages = paginator.page(page)
+    except PageNotAnInteger:
+       ppages = paginator.page(1)
+    except EmptyPage:
+        ppages = paginator.page(paginator.num_pages)
+
+    forms = EmailSignupForm()
+
+    return render(request, 'home_page.html', {
+        'pages': pages,
+        'ppages': ppages,
+        'forms': forms
+    })
+
+
 def list_home(request):
     pages = Home_Page.objects.order_by('date_time')
     query = request.GET.get('q')
