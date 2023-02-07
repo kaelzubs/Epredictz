174f80e972addd2ce2560269e179d267438075e9
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-08 00:28:33 +0100

    Update views.py

--- epz1/views.py
+++ epz1/views.py
@@ -3,137 +3,43 @@ from . models import Home_Page
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Q
 from epz7.forms import EmailSignupForm
+from django.views.generic.dates import DayArchiveView
 
 
-def list_home_prev(request):
-    pages = Home_Page.objects.order_by('date_time')
-    query = request.GET.get('q')
-    if query:
-        pages = Home_Page.objects.filter(
-            Q(date_time__icontains=query) |
-            Q(league__icontains=query) |
-            Q(home_team__icontains=query) |
-            Q(away_team__icontains=query) |
-            Q(tip__icontains=query) |
-            Q(tip_odd__icontains=query) |
-            Q(result__icontains=query)
+class ArticleDayArchiveView(DayArchiveView):
+    pages = Home_Page.objects.all()
+    date_field = "date_time"
+    allow_future = True
 
-        ).distinct()
+    #query = request.GET.get('q')
+    #if query:
+    #    pages = Home_Page.objects.filter(
+    #        Q(date_time__icontains=query) |
+    #        Q(league__icontains=query) |
+    #        Q(home_team__icontains=query) |
+    #        Q(away_team__icontains=query) |
+    #        Q(tip__icontains=query) |
+    #        Q(tip_odd__icontains=query) |
+    #        Q(result__icontains=query)
+    #
+    #    ).distinct()
+    #
+    #paginator = Paginator(pages, 5)
+    #page = request.GET.get('page')
+    #try:
+    #    ppages = paginator.page(page)
+    #except PageNotAnInteger:
+    #   ppages = paginator.page(1)
+    #except EmptyPage:
+    #    ppages = paginator.page(paginator.num_pages)
 
-    paginator = Paginator(pages, 5)
-    page = request.GET.get('page')
-    try:
-        ppages = paginator.page(page)
-    except PageNotAnInteger:
-       ppages = paginator.page(1)
-    except EmptyPage:
-        ppages = paginator.page(paginator.num_pages)
+    #forms = EmailSignupForm()
 
-    forms = EmailSignupForm()
-
-    return render(request, 'home_page.html', {
-        'pages': pages,
-        'ppages': ppages,
-        'forms': forms
-    })
-
-def list_home_today(request):
-    pages = Home_Page.objects.order_by('date_time')
-    query = request.GET.get('q')
-    if query:
-        pages = Home_Page.objects.filter(
-            Q(date_time__icontains=query) |
-            Q(league__icontains=query) |
-            Q(home_team__icontains=query) |
-            Q(away_team__icontains=query) |
-            Q(tip__icontains=query) |
-            Q(tip_odd__icontains=query) |
-            Q(result__icontains=query)
-
-        ).distinct()
-
-    paginator = Paginator(pages, 5)
-    page = request.GET.get('page')
-    try:
-        ppages = paginator.page(page)
-    except PageNotAnInteger:
-       ppages = paginator.page(1)
-    except EmptyPage:
-        ppages = paginator.page(paginator.num_pages)
-
-    forms = EmailSignupForm()
-
-    return render(request, 'home_page.html', {
-        'pages': pages,
-        'ppages': ppages,
-        'forms': forms
-    })
-
-
-def list_home(request):
-    pages = Home_Page.objects.order_by('date_time')
-    query = request.GET.get('q')
-    if query:
-        pages = Home_Page.objects.filter(
-            Q(date_time__icontains=query) |
-            Q(league__icontains=query) |
-            Q(home_team__icontains=query) |
-            Q(away_team__icontains=query) |
-            Q(tip__icontains=query) |
-            Q(tip_odd__icontains=query) |
-            Q(result__icontains=query)
-
-        ).distinct()
-
-    paginator = Paginator(pages, 5)
-    page = request.GET.get('page')
-    try:
-        ppages = paginator.page(page)
-    except PageNotAnInteger:
-       ppages = paginator.page(1)
-    except EmptyPage:
-        ppages = paginator.page(paginator.num_pages)
-
-    forms = EmailSignupForm()
-
-    return render(request, 'home_page.html', {
-        'pages': pages,
-        'ppages': ppages,
-        'forms': forms
-    })
-
-def list_home_next(request):
-    pages = Home_Page.objects.order_by('date_time')
-    query = request.GET.get('q')
-    if query:
-        pages = Home_Page.objects.filter(
-            Q(date_time__icontains=query) |
-            Q(league__icontains=query) |
-            Q(home_team__icontains=query) |
-            Q(away_team__icontains=query) |
-            Q(tip__icontains=query) |
-            Q(tip_odd__icontains=query) |
-            Q(result__icontains=query)
-
-        ).distinct()
-
-    paginator = Paginator(pages, 5)
-    page = request.GET.get('page')
-    try:
-        ppages = paginator.page(page)
-    except PageNotAnInteger:
-       ppages = paginator.page(1)
-    except EmptyPage:
-        ppages = paginator.page(paginator.num_pages)
-
-    forms = EmailSignupForm()
-
-    return render(request, 'home_page.html', {
-        'pages': pages,
-        'ppages': ppages,
-        'forms': forms
-    })
-
+    #return render(request, 'home_page.html', {
+    #    'pages': pages,
+    #    'ppages': ppages,
+    #    'forms': forms
+    #})
 
 def handler404(request, exception, template_name="error_404.html"):
     pages = Home_Page.objects.all()
