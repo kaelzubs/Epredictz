afb037d62d46aa7426ffc9d32ccd5f7c39d02abc
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-08 08:45:30 +0100

    Update urls.py
    (

--- epz1/urls.py
+++ epz1/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import ArticleDayArchiveView
+from .views import list_home, ArticleDayArchiveView
 
 urlpatterns = [
-    #path('', list_home, name='list_home'),
+    path('', list_home, name='list_home'),
     path('<int:year>/<str:month>/<int:day>/', ArticleDayArchiveView.as_view(), name='archive_day'),
 ]
