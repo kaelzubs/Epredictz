35fa51948b5913dcb70426a8f391d1baeb33aaa8
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-09 09:37:25 +0100

    Update forms.py

--- epz1/forms.py
+++ epz1/forms.py
@@ -6,7 +6,7 @@ class DateInput(forms.DateInput):
     input_type = 'date
 
 class LastActiveForm(forms.Form):
-    pub_date = forms.DateField(widget=DateInput)
+    date_added = forms.DateField(widget=DateInput)
     class Meta:
         model = Home_Page
         fields = ('pub_date')
