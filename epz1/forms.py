f4eabd9a37edd20a1ff14ac1e581cd5c54247a58
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-09 02:57:00 +0100

    Update forms.py

--- epz1/forms.py
+++ epz1/forms.py
@@ -6,10 +6,7 @@ class DateInput(forms.DateInput):
     input_type = 'date
 
 class LastActiveForm(forms.Form):
-    """
-    Last Active Date Form
-    """
     pub_date = forms.DateField(widget=DateInput)
     class Meta:
         model = Home_Page
-        fields = ('date_tme', 'league','home_team', 'away_team', 'tip', 'tip_odd', 'result', 'slug', 'pub_date')
+        fields = ('pub_date')
