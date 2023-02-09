3f0cd0d6db4775edc53fc604ef1db5b416d56f2c
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-09 02:36:08 +0100

    Update forms.py

--- epz1/forms.py
+++ epz1/forms.py
@@ -1,1 +1,11 @@
+from django.form import forms
 
+
+class DateInput(forms.DateInput):
+    input_type = 'date
+
+class LastActiveForm(forms.Form):
+    """
+    Last Active Date Form
+    """
+    last_active = forms.DateField(widget=DateInput)
