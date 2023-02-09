225fdc6724d1c4fbc5ab11e80201665bc3f1bdf3
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-09 15:57:55 +0100

    Update views.py

--- epz1/views.py
+++ epz1/views.py
@@ -3,12 +3,12 @@ from . models import Home_Page
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Q
 from epz7.forms import EmailSignupForm
-from datetime import timedelta
+from datetime import timedelta, datetime
 
 
 def list_home(request):
     pages = Home_Page.objects.filter(
-        created_at__date=pub_date - timedelta(1)
+        created_at__date=datetime.now - timedelta(1)
     )
     query = request.GET.get('q')
     if query:
