7914ee18d07f85c961b937826418909a7a24f246
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-09 15:07:14 +0100

    Update urls.py

--- epz1/urls.py
+++ epz1/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import list_home, list_home_yesterday, list_home_today, list_home_tomorrow
+from .views import list_home, list_home_yesterday
 
 urlpatterns = [
     path('', list_home, name='list_home'),
