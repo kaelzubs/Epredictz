bf6e8853bfdcfa855bea979db3530ae25bebf25c
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-19 03:40:56 +0100

    Update forms.py

--- epz1/forms.py
+++ epz1/forms.py
@@ -1,18 +1,5 @@
 from django import forms
-from django.forms import ModelForm
-
-from .models import Home_Page
 
 
-class DateInput(forms.DateInput):
-    input_type = 'date'
-
-
-class SeaechForm(ModelForm):
-
-    class Meta:
-        model = Home_Page
-        fields = ['date_time']
-        widgets = {
-            'date_time': DateInput(),
-        }
+class PickDateForm(forms.Form):
+    date = forms.DateField(widget=forms.DateInput(attrs={'class': 'any_custom_class_name'}))
