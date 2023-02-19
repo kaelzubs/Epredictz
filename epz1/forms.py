885406852b6d10435c3b8c901f197d09efee8ecd
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-19 03:57:50 +0100

    Update forms.py

--- epz1/forms.py
+++ epz1/forms.py
@@ -1,5 +1,9 @@
 from django import forms
+from . models import Home_Page
 
 
 class PickDateForm(forms.Form):
-    date = forms.DateField(widget=forms.DateInput(attrs={'class': 'any_custom_class_name'}))
+    date_time = forms.DateField(widget=forms.DateInput(attrs={'class': 'any_custom_class_name'}))
+    class Meta:
+        model = Home_Page
+        fields = ['date_time']
