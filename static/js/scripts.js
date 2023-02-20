cbf97b2844cc44e20f9fe994bbba8714199a7922
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-20 16:21:35 +0100

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -42,6 +42,18 @@ $(document).ready(function() {
     // document ready  
 });
 
+
+var btn = document.getElementById("btn");
+// the counter
+var counter = document.getElementById("btn-counter");
+// set the counter to 0
+counter.innerHTML = 0;
+
+// increment the counter by 1 everytime the user clicks on the button
+btn.onclick = function() {
+    counter.innerHTML++;
+};
+
  
  
  
