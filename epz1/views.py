ca9860018ea48dd74119bed8b1a35ccc5fb94564
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-21 21:17:56 +0100

    Update views.py

--- epz1/views.py
+++ epz1/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from . models import Home_Page
+from . models import Home_Page, IpModel
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Q
 from epz7.forms import EmailSignupForm
