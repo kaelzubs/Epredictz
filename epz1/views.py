b44eee3aea6b1dc59a943b3a89ae6e5df30e784f
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-22 00:55:57 +0100

    Update views.py

--- epz1/views.py
+++ epz1/views.py
@@ -28,8 +28,56 @@ def get_client_ip(request):
     
     return ip
 
+def list_detail(request, pk):
+    pages = Home_Page.objects.filter(
+        pub_date=datetime.now()
+    )
 
-def list_home(request, pk):
+    vote_id = request.POST.get('vote-id')
+    post = Home_Page.objects.get(pk=vote_id)
+    ip = get_client_ip(request)
+    if not IpModel.objects.filter(ip=ip).exists():
+        IpModel.objects.create(ip=ip)
+    if post.vote.filter(id=IpModel.objects.get(ip=ip).id).exists():
+        post.vote.remove(IpModel.objects.get(ip=ip))
+    else:
+        post.vote.add(IpModel.objects.get(ip=ip))
+
+    return HttpResponseRedirect(reverse('list_home', args=[vote_id]))
+
+    query = request.GET.get('q')
+    if query:
+        pages = Home_Page.objects.filter(
+            Q(pub_date__icontain=query) |
+            Q(date_time__icontains=query) |
+            Q(league__icontains=query) |
+            Q(home_team__icontains=query) |
+            Q(away_team__icontains=query) |
+            Q(tip__icontains=query) |
+            Q(tip_odd__icontains=query) |
+            Q(result__icontains=query)
+    
+        ).distinct()
+ 
+    paginator = Paginator(pages, 10)
+    page = request.GET.get('page')
+    try:
+        ppages = paginator.page(page)
+    except PageNotAnInteger:
+       ppages = paginator.page(1)
+    except EmptyPage:
+        ppages = paginator.page(paginator.num_pages)
+
+    forms = EmailSignupForm()
+
+    return render(request, 'home_page.html', {
+        'pages': pages,
+        'ppages': ppages,
+        'forms': forms
+    })
+
+
+def list_home(request):
     pages = Home_Page.objects.filter(
         pub_date=datetime.now()
     )
