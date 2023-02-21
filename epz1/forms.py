641e3e8c86b28eab228afd127acac1043512ab8c
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-21 19:04:13 +0100

    Delete forms.py

--- epz1/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from . models import Home_Page
-
-
-class PickDateForm(forms.Form):
-    date_time = forms.DateField(widget=forms.DateInput(attrs={'class': 'any_custom_class_name'}))
-    class Meta:
-        model = Home_Page
-        fields = ['date_time']
