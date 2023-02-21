f1cf9022ed5eada8cbb84511e451dc9d75ce2e4a
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-21 18:47:31 +0100

    Update views.py

--- epz1/views.py
+++ epz1/views.py
@@ -17,6 +17,15 @@ def list_calender(request, year, month, day):
         'cal': cal
     })
 
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    
+    return ip
+
 
 def voteLike(request, pk):
     vote_id = request.POST.get('vote-id')
