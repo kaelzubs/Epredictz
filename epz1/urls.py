2eb654062fdf2e547fc0ecf6e3a22f8353f7f440
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-22 01:26:12 +0100

    Update urls.py

--- epz1/urls.py
+++ epz1/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path, re_path
 from .views import list_calender, list_home, list_detail, list_home_today, list_home_yesterday, list_home_tomorrow
-from django.shortcuts import redirect
 
 
 urlpatterns = [
-    path('', lambda req: redirect('/home/')),
+    path('', list_home, name="list_home"),
+    path('vote/<int:pk>/', list_detail, name="list_detail"),
     path('today/', list_home_today, name='list_home_today'),
     path('yesterday/', list_home_yesterday, name='list_home_yesterday'),
     path('tomorrow/', list_home_tomorrow, name='list_home_tomorrow'),
-    path('<int:year>/<str:month>/<int:day>/', list_calender, name='calender'),
-    path('home/', list_home, name="list_home"),
-    path('vote/<int:pk>', list_detail, name="list_detail")
+    path('<int:year>/<str:month>/<int:day>/', list_calender, name='calender')
 ]
