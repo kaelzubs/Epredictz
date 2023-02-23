58e61ebf2436579e91daa454ffed02185a1876d7
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-23 17:17:03 +0100

    Update forms.py

--- epz1/forms.py
+++ epz1/forms.py
@@ -3,7 +3,7 @@ from .models import Home_Page
 from django import forms
 
 
-class EventForm(forms.ModelForm):
+class DateEventForm(forms.ModelForm):
     class Meta:
         model = Home_Page
         fields = "__all__"
