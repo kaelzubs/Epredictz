3970ad1fe637bec3374da4bece2a2888c4619479
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-23 16:22:14 +0100

    Create forms.py

---
+++ epz1/forms.py
@@ -0,0 +1,12 @@
+from django_flatpickr.widgets import DatePickerInput
+from .models import Home_Page
+from django import forms
+
+
+class EventForm(forms.ModelForm):
+    class Meta:
+        model = Home_Page
+        fields = ["__all__"]
+        widgets = {
+            "pub_date": DatePickerInput(),
+        }
