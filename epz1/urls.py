f0ae665222de51e570e9e6581ebdc665e962540a
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-24 02:36:43 +0100

    Update urls.py

--- epz1/urls.py
+++ epz1/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import vote_up, vote_down, list_home, list_home_today, list_home_yesterday, list_home_tomorrow
+from .views import calendar_view, vote_up, vote_down, list_home, list_home_today, list_home_yesterday, list_home_tomorrow
 
 
 urlpatterns = [
     path('', list_home, name="list_home"),
+    path('<int:year>/<str:month>/<int:day>/', calendar_view, name='calendar'),
     path('likes-prediction/<int:pk>/', vote_up, name='like'),
     path('dislikes-prediction/<int:pk>/', vote_down, name='dislike'),
     path('today/', list_home_today, name='list_home_today'),
