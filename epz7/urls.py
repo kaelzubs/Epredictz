3ec6e549a34b454644d1cfc271d9be7f899bdc17
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-25 15:30:43 +0100

    Update urls.py

--- epz7/urls.py
+++ epz7/urls.py
@@ -4,5 +4,5 @@ from .views import email_list_signup, sub_success
 
 urlpatterns = [
     path('page/', email_list_signup, name='email_list'),
-    path('sub_success', sub_success, name='sub_success')
+    path('sub-successful/', sub_success, name='sub_success')
 ]
