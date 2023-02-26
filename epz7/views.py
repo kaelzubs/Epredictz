012ef1ef911ee326e1940ac16221de33d45eb6e4
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-26 14:14:31 +0100

    Update views.py

--- epz7/views.py
+++ epz7/views.py
@@ -27,53 +27,18 @@ def subscribe(email):
     return r.status_code, r.json()
 
 def email_list_signup(request):
-    pages = Home_Page.objects.filter(
-        pub_date=datetime.now()
-    )
-    query = request.GET.get('q')
-    if query:
-        pages = Home_Page.objects.filter(
-            Q(pub_date__icontains=query) |
-            Q(date_time__icontains=query) |
-            Q(league__icontains=query) |
-            Q(home_team__icontains=query) |
-            Q(away_team__icontains=query) |
-            Q(tip__icontains=query) |
-            Q(tip_odd__icontains=query) |
-            Q(result__icontains=query)
-    
-        ).distinct()
-
     form = EmailSignupForm(request.POST or None)
     if request.method == 'POST':
         if form.is_valid():
             email_signup_qs = Sign_up.objects.filter(email=form.instance.email)
             if email_signup_qs.exists():
-                return render(request, 'subscribed.html', {'form': form, 'pages': pages})
+                return render(request, 'subscribed.html', {'form': form})
             else:
                 subscribe(form.instance.email)
                 form.save()
-            # return redirect('sub_success')
-
+                
+            redirect('sub_success')
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
-
 def sub_success(request):
-    pages = Home_Page.objects.filter(
-        pub_date=datetime.now()
-    )
-    query = request.GET.get('q')
-    if query:
-        pages = Home_Page.objects.filter(
-            Q(pub_date__icontains=query) |
-            Q(date_time__icontains=query) |
-            Q(league__icontains=query) |
-            Q(home_team__icontains=query) |
-            Q(away_team__icontains=query) |
-            Q(tip__icontains=query) |
-            Q(tip_odd__icontains=query) |
-            Q(result__icontains=query)
-    
-        ).distinct()
-
-    return render(request, 'sub_success.html', {'pages': pages})
+    return render(request, 'sub_success.html', {})
