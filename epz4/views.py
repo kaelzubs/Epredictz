3a756152508e3a34fc8331d6320ca94d427542dd
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-26 10:47:25 +0100

    Update views.py

--- epz4/views.py
+++ epz4/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from epz7.forms import EmailSignupForm
 # Create your views here.
 
 def list_disclaim(request):
-    return render(request, 'disclaimer_page.html', {})
+    forms = EmailSignupForm()
+    return render(request, 'disclaimer_page.html', {'forms': forms})
