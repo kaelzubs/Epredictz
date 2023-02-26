5af2df868797d75c3f19fae501c7f02977138a9d
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-26 11:57:25 +0100

    Update views.py

--- epz5/views.py
+++ epz5/views.py
@@ -3,5 +3,26 @@ from epz7.forms import EmailSignupForm
 # Create your views here.
 
 def list_faqs(request):
+    pages = Home_Page.objects.filter(
+        pub_date=datetime.now()
+    )
+    query = request.GET.get('q')
+    if query:
+        pages = Home_Page.objects.filter(
+            Q(pub_date__icontains=query) |
+            Q(date_time__icontains=query) |
+            Q(league__icontains=query) |
+            Q(home_team__icontains=query) |
+            Q(away_team__icontains=query) |
+            Q(tip__icontains=query) |
+            Q(tip_odd__icontains=query) |
+            Q(result__icontains=query)
+    
+        ).distinct()
+
     forms = EmailSignupForm()
-    return render(request, 'faq_page.html', {'forms': forms})
+
+    return render(request, 'faq_page.html', {
+        'forms': forms,
+        'pages': pages
+    })
