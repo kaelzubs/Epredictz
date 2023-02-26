42b2559890b6fd67fec2ff943d2e006c8385702a
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-26 10:50:20 +0100

    Update views.py

--- epz6/views.py
+++ epz6/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from epz7.forms import EmailSignupForm
 # Create your views here.
 
 def cookie_policy(request):
-    return render(request, 'cookielaw/cookie_page.html', {})
+    forms = EmailSignupForm()
+    return render(request, 'cookielaw/cookie_page.html', {'forms': forms})
