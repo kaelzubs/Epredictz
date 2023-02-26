8f06c13bde11b96fc249dca8c7bafb3c4de877c8
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-26 10:36:54 +0100

    Update views.py

--- epz2/views.py
+++ epz2/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from epz7.forms import EmailSignupForm
 # Create your views here.
 
 def list_about(request):
-    return render(request, 'about_page.html', {})
+    forms = EmailSignupForm()
+    return render(request, 'about_page.html', {
+        'forms': forms
+    })
