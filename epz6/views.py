0c7cec76e5055f136a17a61d0883d9c8507cef25
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-02-26 16:24:40 +0100

    Update views.py

--- epz6/views.py
+++ epz6/views.py
@@ -21,9 +21,19 @@ def cookie_policy(request):
     
         ).distinct()
 
+    paginator = Paginator(pages, 10)
+    page = request.GET.get('page')
+    try:
+        ppages = paginator.page(page)
+    except PageNotAnInteger:
+       ppages = paginator.page(1)
+    except EmptyPage:
+        ppages = paginator.page(paginator.num_pages)
+
     forms = EmailSignupForm()
 
     return render(request, 'cookielaw/cookie_page.html', {
         'forms': forms,
-        'pages': pages
+        'pages': pages,
+        'ppages': ppages
     })
