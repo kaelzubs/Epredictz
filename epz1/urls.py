0d00898d4773d74ca4ef7618440701fcae9921f9
Elvis chukwuka <37744693+kaelzubs@users.noreply.github.com>
2023-03-08 21:12:55 +0100

    Update urls.py

--- epz1/urls.py
+++ epz1/urls.py
@@ -4,10 +4,10 @@ from .views import vote_up, vote_down, list_home, list_home_today, list_home_yes
 
 urlpatterns = [
     path('', list_home, name="list_home"),
-    path('likes-prediction/<int:pk>/', vote_up, name='like'),
-    path('dislikes-prediction/<int:pk>/', vote_down, name='dislike'),
-    path('today/', list_home_today, name='list_home_today'),
-    path('yesterday/', list_home_yesterday, name='list_home_yesterday'),
-    path('tomorrow/', list_home_tomorrow, name='list_home_tomorrow'),
-    path('all-prediction-list/', list_all_prediction, name='list_all')
+    path('likes-football-prediction/<int:pk>/', vote_up, name='like'),
+    path('dislikes-football-prediction/<int:pk>/', vote_down, name='dislike'),
+    path('today-fooball-prediction/', list_home_today, name='list_home_today'),
+    path('yesterday-fooball-prediction/', list_home_yesterday, name='list_home_yesterday'),
+    path('tomorrow-fooball-prediction/', list_home_tomorrow, name='list_home_tomorrow'),
+    path('all-football-prediction-list/', list_all_prediction, name='list_all')
 ]
